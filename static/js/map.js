edb211cf59686de1fc3369a3bd673bf34de146b0
Akshara08 <68265306+Akshara082000@users.noreply.github.com>
2020-07-15 21:27:20 +0530

    Add files via upload

---
+++ static/js/map.js
@@ -0,0 +1,29 @@
+
+var map=L.map('map').setView([22.910502,75.989501],5);
+L.tileLayer("https://api.maptiler.com/maps/hybrid/256/{z}/{x}/{y}.jpg?key=Gb2ovo1WytzyxeUcRP7b",{
+    /*attribution:'<a href="https://www.maptiler.com/copyright/" target="_blank">&copy; MapTiler</a> <a href="https://www.openstreetmap.org/copyright" target="_blank">&copy; OpenStreetMap contributors</a>'*/
+}).addTo(map);
+
+L.marker([22.910502,75.989501],15).addTo(map)
+    .bindPopup('Click on the states <br>of the country<br>to get accurate results.')
+    .openPopup();
+
+map.on('click', function(e) {
+console.log("Lat, Lon : " + e.latlng.lat + ", " + e.latlng.lng);
+$.ajax({ url:'https://www.mapquestapi.com/geocoding/v1/reverse?key=cJ7E9WnLGHbfVncS0U3InQGKRAESPRyA&location='+e.latlng.lat+','+e.latlng.lng+'&outFormat=json&thumbMaps=false',
+success: function(data){
+    var state = data.results[0].locations[0].adminArea3;
+    var country=data.results[0].locations[0].adminArea1;
+    var city=data.results[0].locations[0].adminArea5;
+    console.log(data);
+    console.log(state+","+country);
+document.getElementById("lang").value=e.latlng.lng;
+document.getElementById("lat").value=e.latlng.lat;
+document.getElementById("Country").value=city+","+state+","+country;
+}
+});
+});
+
+
+
+
